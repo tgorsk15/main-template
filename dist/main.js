ccf87a16a2da81df2cfed2293121e7edcacde115
tgorsk15 <tgbusiness463@gmail.com>
2024-01-21 14:46:44 -0600

    Add webpack, .gitignore, package.json file to template

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Why are we still here?')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbWFpbi10ZW1wbGF0ZS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnV2h5IGFyZSB3ZSBzdGlsbCBoZXJlPycpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log('Why are we still here?')
